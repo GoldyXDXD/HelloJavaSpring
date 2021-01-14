73e77ef49bb99bc74983b0abe33f0e672cda855b
GoldyXDXD <62290862+GoldyXDXD@users.noreply.github.com>
2021-01-14 22:07:45 +0300

    Created RegistrationController.java

---
+++ src/main/java/org.teamnescafe.demo/controllers/RegistrationController.java
@@ -0,0 +1,32 @@
+package com.nickbor.demo.controllers;
+
+import com.nickbor.demo.models.Role;
+import com.nickbor.demo.models.User;
+import com.nickbor.demo.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Model model) {
+        User userFromDb = userRepository.findbyUsername(user.getUsername());
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
